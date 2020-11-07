0d49f267a3dedba909e3fe24bfb6dc4929f87947
Sofiane <sofiane.benembareck@hotmail.com>
2020-11-07 17:17:18 +0100

    Implem du modèle linéaire Rosenblatt (pas sur qu'il soit bon...)
    Implémentation du model de rosenblatt, mais pas sur qu'il soit correct :/
    Rangement des scripts etc

--- ML_Dll_project/MLDLL/Source.cpp
+++ ML_Dll_project/MLDLL/Source.cpp
@@ -5,16 +5,108 @@
 //permet d'attacher la source.cpp à unity et de voir step by step
 //ce qu'il se passe
 
-extern "C" {
+extern "C"
+{
+#pragma region Linear Model
 
-	struct test
+	__declspec(dllexport) double* create_linear_model(int input_counts)
 	{
-		int a;
-		int b;
+		//On crée un tableau de input + 1 pour inclure le biais
+		auto weights = new double[input_counts + 1];
 
-		std::vector<double> w;
-	};
+		//Initialise le model avec des poids random selon le nombre d'input
+		for (auto i = 0; i < input_counts + 1; ++i)
+		{
+			weights[i] = rand() / static_cast<double>(RAND_MAX) * 2.0 - 1.0;
+		}
 
+		return weights;
+	}
+
+	__declspec(dllexport) double* predict_linear_model_multiclass(double* model, double inputs[], int input_count, int class_count, bool is_classification)
+	{
+		// todo
+
+		return new double[3]{ 1.0, 1.0, -1.0 };
+	}
+
+	__declspec(dllexport) double predict_linear_model(double* model, double inputs[], int input_count, int sample_count, bool is_classification)
+	{
+		double sum = 0.0;
+
+		for(int n = 0; n < sample_count; ++n)
+		{
+			//Somme pondéré des poinds * inputs
+			for(int i = 0; i < input_count; ++i)
+			{
+				sum += model[i] * inputs[n];
+			}
+		}
+
+		if (is_classification)
+			return sum > 0.0 ? 1.0
+							: sum < 0.0 ? -1.0
+							: 0.0;
+		else
+			return sum;
+	}
+
+
+	__declspec(dllexport) void train_linear_model_regression(double* model, double all_inputs[], int input_count,
+		int sample_counts, double all_expected_outputs[], int expected_output_count)
+	{
+
+	}
+	
+	__declspec(dllexport) void train_linear_model_rosenblatt(double* model, double all_inputs[], int input_count,
+		int sample_counts, double all_expected_outputs[], int expected_output_count, int epochs, double learning_rate)
+	{
+		for(int it = 0; it < epochs; ++it)
+		{
+			const int max = sample_counts;
+			const int min = 0;
+			const int k = rand() % (max - min) + min;
+
+			const int x_k_length = sample_counts * (k + 1) - (sample_counts * k);
+			std::vector<double> x_k;
+			x_k.reserve(x_k_length);
+
+			int start = sample_counts * k;
+			int end = sample_counts * (k + 1);
+			for (int i = start; i < end; ++i)
+			{
+				x_k.emplace_back(all_inputs[i]);
+			}
+
+			const int y_k_length = expected_output_count * (k + 1) - (expected_output_count * k);
+			std::vector<double> y_k;
+			y_k.reserve(y_k_length);
+
+			start = expected_output_count * k;
+			end = expected_output_count * (k + 1);
+			for (int i = start; i < end; ++i)
+			{
+				y_k.emplace_back(all_expected_outputs[i]);
+			}
+
+			auto g_Xk = predict_linear_model(model, x_k.data(), input_count, sample_counts, true);
+
+			for(int n = 0; n < sample_counts; ++n)
+				for(int l = 0; l < expected_output_count; ++l)
+					for(int i = 0; i < input_count; ++i)
+						model[i] = model[i] + learning_rate * (y_k[l] - g_Xk) * x_k[n];
+				
+		}
+	}
+
+	
+	__declspec(dllexport) void delete_linear_model(double* model)
+	{
+		delete[] model;
+	}
+#pragma endregion 
+
+#pragma region MLP
 	struct MLP
 	{
 		/// <summary>
@@ -45,86 +137,6 @@ extern "C" {
 		/// </summary>
 		std::vector< std::vector<double> > deltas;
 	};
-
-	//npl ==> [input: 3, hidden: 5,5,5, out: 1], doit etre donnée a chaque fois
-	//layer_number = 5, doit etre donnée a chaque fois
-	//L = layer number - 1, créer a chaque fois
-	//deltas peut etre déclaré et construit dans la fonction train
-	//x peut etre construit dans train et passé en paramettre à predict
-	//w est toujours return a unity afin de pouvoir le re utiliser
-
-	__declspec(dllexport) test* create_test(int a, int b)
-	{
-		auto w = new test;
-
-		w->a = a;
-		w->b = b;
-
-		w->w.emplace_back(33);
-
-		return w;
-	}
-
-	__declspec(dllexport) double* create_linear_model(int input_counts)
-	{
-		//On crée un tableau de input + 1 pour inclure le biais
-		auto weights = new double[input_counts + 1];
-
-		//Initialise le model avec des poids random selon le nombre d'input
-		for (auto i = 0; i < input_counts + 1; ++i)
-		{
-			weights[i] = rand() / static_cast<double>(RAND_MAX) * 2.0 - 1.0;
-		}
-
-		return weights;
-	}
-
-	__declspec(dllexport) double* predict_linear_model_multiclass(double* model, double inputs[], int input_count, int class_count, bool is_classification)
-	{
-		// todo
-
-		return new double[3]{ 1.0, 1.0, -1.0 };
-	}
-
-	__declspec(dllexport) double predict_linear_model(double* model, double inputs[], int input_count, bool is_classification)
-	{
-		double result = 0.0;
-
-		//model contient les poids
-		int d[2];
-		int L = 2;
-
-		for (int l = 1; l < L + 1; ++l)
-		{
-			for (int j = 1; j < d[l] + 1; ++j)
-			{
-				double sum = 0.0;
-
-				for (int i = 0; i < d[l - 1] + 1; ++i)
-				{
-					tanh(sum);
-				}
-			}
-		}
-
-		return result;
-	}
-
-	__declspec(dllexport) void train_linear_model_rosenblatt(double* model, double all_inputs[], int input_count,
-		int sample_couts, double all_expected_outputs[], int expected_output_count, int epochs, double learning_rate, bool is_classification)
-	{
-		// todo
-
-		return;
-	}
-
-	__declspec(dllexport) void delete_linear_model(double* model)
-	{
-		delete[] model;
-	}
-
-
-
 
 	__declspec(dllexport) MLP* create_model(int npl[], int layer_counts)
 	{
@@ -239,7 +251,7 @@ extern "C" {
 	__declspec(dllexport) double* predict(MLP* model, double inputs[], bool isClass)
 	{
 		forward_pass(model, inputs, isClass);
-		
+
 		double* d = new double[model->x[model->L].size()];
 
 		for (int i = 0; i < model->x[model->L].size(); ++i)
@@ -247,7 +259,7 @@ extern "C" {
 
 		return d;
 	}
-	
+
 	__declspec(dllexport) void train(MLP* model, double allInputs[], double allExpectedOutputs[],
 		int sampleCount, int epochs, double alpha, bool isClassification)
 	{
@@ -270,7 +282,6 @@ extern "C" {
 				// y_k = all_expected_outputs[outputs_size * k:outputs_size * (k + 1)]
 
 				const int x_k_length = inputsSize * (k + 1) - (inputsSize * k);
-				//double* x_k = new double[x_k_length]; // PAS SUR
 				std::vector<double> x_k;
 				x_k.reserve(x_k_length);
 
@@ -282,7 +293,6 @@ extern "C" {
 				}
 
 				const int y_k_length = outputsSize * (k + 1) - (outputsSize * k);
-				//double* y_k = new double[y_k_length]; // PAS SUR
 				std::vector<double> y_k;
 				y_k.reserve(y_k_length);
 
@@ -293,7 +303,7 @@ extern "C" {
 					y_k.emplace_back(allExpectedOutputs[i]);
 				}
 
- 				forward_pass(model, x_k.data(), isClassification);
+				forward_pass(model, x_k.data(), isClassification);
 
 				for (int j = 1; j < model->d[model->L] + 1; ++j)
 				{
@@ -336,13 +346,14 @@ extern "C" {
 
 		delete model;
 	}
+#pragma endregion 
 
 	__declspec(dllexport) void delete_double_array_ptr(double* ptr)
 	{
 		delete[] ptr;
 	}
 
-
+#pragma region Fonctions de test
 	//__declspec(dllexport) Permet de spécifier, pour windows, que cette fonction va en dll
 	//l'interface d'une fonction doit respecter les code C
 	//mais son contenu peut faire appel à du code C++ / C / Other
@@ -355,4 +366,5 @@ extern "C" {
 	{
 		return t->L + 0;
 	}
+#pragma endregion
 }
